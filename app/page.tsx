12cb7f29d2ce629fa214bd2a1ac3193bcdb91fe5
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2023-11-08 22:55:36 +0000

    made pointers bigger

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,25 @@ export default function Home() {
       <div className="flex justify-center text-center">
         <h2 className="grid scroll-m-20 pb-2 text-3xl md:text-6xl xl:text-8xl font-semibold tracking-tight first:mt-0">
           <p className="pb-10 opacity-60 hover:opacity-100 hover:text-5xl hover:md:text-7xl hover:xl:text-9xl hover:font-extrabold transition-all min-h-[60dvh]">
-            Hi, I&apos;m Kaleb.&#128075;
+            Hi, I&apos;m Kaleb. &#128075;
           </p>
           <a
             href="#about"
-            className="text-xl min-h-[40dvh] opacity-40 hover:opacity-100 transition-all"
+            className="text-xl md:text-3xl min-h-[40dvh] opacity-40 hover:opacity-100 transition-all"
           >
             &#128071;
           </a>
           <p className="pb-4 opacity-60 hover:opacity-100 hover:text-5xl hover:md:text-7xl hover:xl:text-9xl hover:font-bold transition-all min-h-screen">
-            Studying...
+            Studying . . .
           </p>
           <p className="pb-2 opacity-60 hover:opacity-100 hover:text-5xl hover:md:text-7xl hover:xl:text-9xl hover:font-bold transition-all min-h-screen">
-            Computer Science, &#128187;
+            Computer Science &#128187;
           </p>
           <p className="pb-2 opacity-60 hover:opacity-100 hover:text-5xl hover:md:text-7xl hover:xl:text-9xl hover:font-bold transition-all min-h-screen">
-            Physics, &#128640;
+            Physics &#128640;
           </p>
           <p className="pb-2 opacity-60 hover:opacity-100 hover:text-5xl hover:md:text-7xl hover:xl:text-9xl hover:font-bold transition-all min-h-screen">
-            Maths. &#129518;
+            Maths &#129518;
           </p>
         </h2>
       </div>
@@ -43,7 +43,7 @@ export default function Home() {
       </div>
       <a
         href="#/"
-        className="text-xl min-h-[40dvh] opacity-40 hover:opacity-100 transition-all flex justify-center"
+        className="text-xl md:text-3xl min-h-[40dvh] opacity-40 hover:opacity-100 transition-all flex justify-center"
       >
         &#128070;
       </a>
