199617fafbffc3b443f2426efa86af47426c8ecd
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2023-11-09 23:00:16 +0000

    added scroll snap

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth snap-y snap-mandatory">
       <head />
       <body className={fontSans.className}>
         <ThemeProvider
--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,42 @@ import { ProjectCards } from "@/components/project-cards";
 export default function Home() {
   return (
     <main id="/" className="flex flex-col justify-center p-8 cursor-default">
-      <div className="flex justify-center text-center">
+      <div className="flex justify-center text-center pb-56">
         <h2 className="grid scroll-m-20 pb-2 text-3xl md:text-6xl xl:text-8xl font-semibold tracking-tight first:mt-0">
-          <p className="pb-10 opacity-60 hover:opacity-100 hover:text-5xl hover:md:text-7xl hover:xl:text-9xl hover:font-extrabold transition-all min-h-[60dvh]">
-            Hi, I&apos;m Kaleb. &#128075;
-          </p>
+          <section className="snap-center">
+            <p className="py-64 text-5xl md:text-7xl xl:text-9xl font-extrabold">
+              Hi, I&apos;m Kaleb. &#128075;
+            </p>
+          </section>
           <a
             href="#about"
-            className="text-xl md:text-3xl min-h-[40dvh] opacity-40 hover:opacity-100 transition-all"
+            className="text-xl md:text-3xl min-h-[80dvh] opacity-40 hover:opacity-100 transition-all"
           >
             &#128071;
           </a>
-          <p className="pb-4 opacity-60 hover:opacity-100 hover:text-5xl hover:md:text-7xl hover:xl:text-9xl hover:font-bold transition-all min-h-screen">
-            Studying . . .
-          </p>
-          <p className="pb-2 opacity-60 hover:opacity-100 hover:text-5xl hover:md:text-7xl hover:xl:text-9xl hover:font-bold transition-all min-h-screen">
-            Computer Science &#128187;
-          </p>
-          <p className="pb-2 opacity-60 hover:opacity-100 hover:text-5xl hover:md:text-7xl hover:xl:text-9xl hover:font-bold transition-all min-h-screen">
-            Physics &#128640;
-          </p>
-          <p className="pb-2 opacity-60 hover:opacity-100 hover:text-5xl hover:md:text-7xl hover:xl:text-9xl hover:font-bold transition-all min-h-screen">
-            Maths &#129518;
-          </p>
+          <section className="snap-center">
+            <p className="py-64 text-5xl md:text-7xl xl:text-9xl font-extrabold">
+              Studying . . .
+            </p>
+          </section>
+          <section className="snap-center">
+            <p className="py-64 text-5xl md:text-7xl xl:text-9xl font-extrabold">
+              Computer Science &#128187;
+            </p>
+          </section>
+          <section className="snap-center">
+            <p className="py-64 text-5xl md:text-7xl xl:text-9xl font-extrabold">
+              Physics &#128640;
+            </p>
+          </section>
+          <section className="snap-center">
+            <p className="py-64 text-5xl md:text-7xl xl:text-9xl font-extrabold">
+              Maths &#129518;
+            </p>
+          </section>
         </h2>
       </div>
-      <div id="about" className="min-h-[60dvh]">
+      <div id="about" className="min-h-[60dvh] snap-center">
         <h2 className="flex justify-center scroll-m-20 pb-2 text-3xl md:text-6xl font-semibold tracking-tight first:mt-0">
           About Me &#128170;
         </h2>
@@ -47,8 +57,8 @@ export default function Home() {
       >
         &#128070;
       </a>
-      <div id="projects">
-        <h2 className="flex justify-center scroll-m-20 pb-2 text-3xl md:text-6xl font-semibold tracking-tight first:mt-0 min-h-[20dvh] animate-bounce">
+      <div id="projects" className="snap-center">
+        <h2 className="flex justify-center scroll-m-20 pb-2 text-3xl md:text-6xl font-semibold tracking-tight first:mt-0 min-h-[20dvh]">
           Projects &#128526;
         </h2>
         <ProjectCards />
