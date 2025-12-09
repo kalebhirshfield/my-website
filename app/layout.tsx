a6d45734f0b14654b173a00c8c0ef775a73f941f
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2025-12-09 12:02:55 +0000

    chore: update dependencies and TypeScript configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head />
       <body className={fontSans.className}>
         <ThemeProvider attribute="class" defaultTheme="dark">
